d9692433d72ea2eb49d1f0b5f5dc4d6fc65a0cd1
Philemon Kirlles <philemon.kirlles@gmail.com>
2022-09-14 11:42:52 -0400

    recursive' strategy cleard my work in a conflict

--- reactPortfolio/models/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Contact: require('./contactModel'),
-  MyProject: require('./projectsModel')
-}
--- reactPortfolio/routes/api/index.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const contactRoutes = require("./contect");
-const projectsRoutes = require('./projects')
-
-router.use("/contect", contactRoutes);
-router.use('/projects', projectsRoutes)
-
-module.exports = router;
